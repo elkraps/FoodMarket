1e3b932a5597ef17ce7670e3eae1e88ae43aa106
elkraps <dobroisreal@mail.ru>
2023-07-06 16:31:01 +0300

    Initial Commit

---
+++ FoodMarket/FoodMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMarketApp.swift
+//  FoodMarket
+//
+//  Created by Vlad Dzirko on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
